5bdbe55fed13c563579ba418c509ff2b606fcae7
wizard-86 <informatikamalam1@gmail.com>
2026-06-14 19:38:55 +0700

    membuat UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin'
+        ]);
+
+        User::create([
+            'name' => 'Rama',
+            'email' => 'rama@gmail.com',
+            'password' => Hash::make('password'),
+            'role' => 'user'
+        ]);
+    }
+    }
+}
